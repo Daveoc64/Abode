71f602930442ce42ab96a115325fe336e2c0952d
James Raspass <jraspass@gmail.com>
2012-12-09 04:17:07 +0000

    basic settings

--- src/me/raspass/abode/Main.java
+++ src/me/raspass/abode/Main.java
@@ -2,6 +2,9 @@ package me.raspass.abode;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.support.v4.app.ActivityCompat;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -15,14 +18,25 @@ public class Main extends Activity implements AdapterView.OnItemClickListener
 	{
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.main );
-        
+
 		gridview = ( GridView ) findViewById( R.id.gridview );
 		gridview.setAdapter( new ImageAdapter( this ) );
 		gridview.setOnItemClickListener(this);
 	}
-    
+
 	public void onItemClick( AdapterView<?> parent, View v, int position, long id )
 	{
 		( ( ImageAdapter ) parent.getAdapter() ).getItem( position );
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu)
+	{
+		new AlertDialog.Builder(this).setView(this.getLayoutInflater().inflate(R.layout.settings, null)).create().show();
+
+		// Ensure future menu presses call this method
+		ActivityCompat.invalidateOptionsMenu(this);
+
+		return true;
 	}
 }
