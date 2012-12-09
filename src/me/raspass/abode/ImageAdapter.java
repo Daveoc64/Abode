95c26063f85157b2ce859a0d0c0b2b2827498507
James Raspass <jraspass@gmail.com>
2012-12-09 04:22:10 +0000

    auto tidy code

--- src/me/raspass/abode/ImageAdapter.java
+++ src/me/raspass/abode/ImageAdapter.java
@@ -13,62 +13,59 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-public class ImageAdapter extends BaseAdapter
-{
+public class ImageAdapter extends BaseAdapter {
 	private Context context;
 	private ArrayList<ResolveInfo> apps = new ArrayList<ResolveInfo>();
 	private PackageManager pm;
-	
-	public ImageAdapter( Context context )
-	{
-		this.context = context;
-		this.pm      = context.getPackageManager();
 
-		Intent intent = new Intent( Intent.ACTION_MAIN, null );
-		       intent.addCategory(  Intent.CATEGORY_LAUNCHER );
-	
-		for ( ResolveInfo app : pm.queryIntentActivities( intent, 0 ) )
-		{
+	public ImageAdapter(Context context) {
+		this.context = context;
+		this.pm = context.getPackageManager();
+
+		Intent intent = new Intent(Intent.ACTION_MAIN, null);
+		intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+		for (ResolveInfo app : pm.queryIntentActivities(intent, 0)) {
 			int i;
 
-			for ( i = 0; i < apps.size(); i++ )
-			{
-				if ( apps.get( i ).loadLabel( pm ).toString().compareTo( app.loadLabel( pm ).toString() ) > 0 ){ break; }
+			for (i = 0; i < apps.size(); i++) {
+				if (apps.get(i).loadLabel(pm).toString()
+						.compareTo(app.loadLabel(pm).toString()) > 0) {
+					break;
+				}
 			}
 
-			this.apps.add( i, app );
+			this.apps.add(i, app);
 		}
 	}
-	
-	public View getView( int position, View convertView, ViewGroup parent )
-	{
-		ResolveInfo app = apps.get( position );
 
-		TextView view = ( TextView ) ( convertView == null ? ( ( LayoutInflater ) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE ) ).inflate( R.layout.icon, null ) : convertView );
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ResolveInfo app = apps.get(position);
 
-		Drawable img = app.loadIcon( pm );
-		img.setBounds( 0, 0, 70, 70 );
-		view.setCompoundDrawables( null, img, null, null );
+		TextView view = (TextView) (convertView == null ? ((LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(
+				R.layout.icon, null) : convertView);
 
-		view.setText( app.loadLabel( pm ) );
+		Drawable img = app.loadIcon(pm);
+		img.setBounds(0, 0, 70, 70);
+		view.setCompoundDrawables(null, img, null, null);
+
+		view.setText(app.loadLabel(pm));
 
 		return view;
 	}
-	
-	public int getCount()
-	{
+
+	public int getCount() {
 		return apps.size();
 	}
- 
-	public Object getItem( int position )
-	{
-		context.startActivity( pm.getLaunchIntentForPackage( apps.get( position ).activityInfo.packageName ) );
+
+	public Object getItem(int position) {
+		context.startActivity(pm.getLaunchIntentForPackage(apps.get(position).activityInfo.packageName));
 
 		return null;
 	}
 
-	public long getItemId( int position )
-	{
+	public long getItemId(int position) {
 		return 0;
 	}
 }
--- src/me/raspass/abode/Main.java
+++ src/me/raspass/abode/Main.java
@@ -9,30 +9,29 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
-public class Main extends Activity implements AdapterView.OnItemClickListener
-{
+public class Main extends Activity implements AdapterView.OnItemClickListener {
 	private GridView gridview;
-	
-	@Override
-	public void onCreate( Bundle savedInstanceState )
-	{
-		super.onCreate( savedInstanceState );
-		setContentView( R.layout.main );
 
-		gridview = ( GridView ) findViewById( R.id.gridview );
-		gridview.setAdapter( new ImageAdapter( this ) );
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		gridview = (GridView) findViewById(R.id.gridview);
+		gridview.setAdapter(new ImageAdapter(this));
 		gridview.setOnItemClickListener(this);
 	}
 
-	public void onItemClick( AdapterView<?> parent, View v, int position, long id )
-	{
-		( ( ImageAdapter ) parent.getAdapter() ).getItem( position );
+	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+		((ImageAdapter) parent.getAdapter()).getItem(position);
 	}
 
 	@Override
-	public boolean onPrepareOptionsMenu(Menu menu)
-	{
-		new AlertDialog.Builder(this).setView(this.getLayoutInflater().inflate(R.layout.settings, null)).create().show();
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		new AlertDialog.Builder(this)
+				.setView(
+						this.getLayoutInflater().inflate(R.layout.settings,
+								null)).create().show();
 
 		// Ensure future menu presses call this method
 		ActivityCompat.invalidateOptionsMenu(this);
